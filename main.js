358bbe19815ea84d703542f431a072b9a6e162c8
jenniferk2019 <51275153+jenniferk2019@users.noreply.github.com>
2019-09-15 19:04:29 -0400

    jquery functions for toggle animation and images

---
+++ main.js
@@ -0,0 +1,36 @@
+$(document).ready(function () {
+    $('.menu-toggler').on('click', function () {
+        $(this).toggleClass('open');
+        $('.top-nav').toggleClass('open');
+    });
+
+    $('.top-nav .nav-link').on('click', function () {
+        $('.menu-toggler').removeClass('open');
+        $('.top-nav').removeClass('open');
+    });
+
+    $('nav a[href*="#"]').on('click', function () {
+        $('html, body').animate({
+            scrollTop: $($(this).attr('href')).offset().top,
+        },
+            200,
+            'linear'
+        );
+    });
+
+    $('#up').on('click', function () {
+        $('html, body').animate({
+            scrollTop: 0
+        },
+            500,
+            'linear'
+        );
+    });
+
+    AOS.init({
+        easing: 'ease',
+        duration: 1800
+    });
+
+},
+);
